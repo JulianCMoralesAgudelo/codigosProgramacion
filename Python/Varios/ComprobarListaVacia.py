82c266ea42fc09cfc43ebe2444c16b6aec00872d
JulianCMoralesAgudelo <juliancmorales10@gmail.com>
2020-11-04 18:55:02 -0500

    batch

---
+++ Python/Varios/ComprobarListaVacia.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Declaracion e inicializacion de variables
+
+# Manipulacion de datos y # Presentacion de datos.
+
+nums = [1, 2, 3]
+if nums:
+    print(nums)
+else:
+    print('La lista está vacía')
+
+nums = []
+if nums:
+    print(nums)
+else:
+    print('La lista está vacía')
+
+
+
+
+
---
+++ Python/Varios/creaCadenas.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+
+# Declaracion e inicializacion de variables
+
+a = 'a'
+b = 'b'
+c = 'c'
+
+# Manipulacion de dato y Presentacion de datos.
+
+for i in range(50, 55):
+    print(a*i+b*(i*2))
