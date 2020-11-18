d6b138ddb7d963f54f80e715fec194729ef8f13c
JulianCMoralesAgudelo <juliancmorales10@gmail.com>
2020-11-18 10:37:47 -0500

    Proyecto PPO2

--- Java/spring-mvc/src/main/java/co/edu/iudigital/app/web/EmployeeRestController.java
+++ Java/spring-mvc/src/main/java/co/edu/iudigital/app/web/EmployeeRestController.java
@@ -6,16 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.iudigital.app.RecordNotFoundException.RecordNotFoundException;
 import co.edu.iudigital.app.model.EmployeeEntity;
 import co.edu.iudigital.app.service.EmployeeService;
 
-@RestController // para colocarlo en el contenedor de Spring e indicar un Controller Rest
-@RequestMapping(value = "/rest")// endpoint
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET})//externos hagan peticion
+@RestController // Para colocarlo en el contenedor de Spring e indicar un Controller Rest
+
+@RequestMapping(value = "/rest") // endpoint
+
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE }) 
+
+
 public class EmployeeRestController {
 
 	// inyección de dependencias
@@ -23,12 +30,32 @@ public class EmployeeRestController {
 	private EmployeeService employeeService;
 
 	// Punto de peticion
-	
-	// punto de petición get
-	@RequestMapping(value = {""}, method = RequestMethod.GET)
+
+	// Punto de petición get
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = { "" }, method = RequestMethod.GET)
 	public ResponseEntity<List<EmployeeEntity>> getAllEmployees() {
-		return new ResponseEntity(employeeService.getAllEmployees(),
-				HttpStatus.OK);
+		return new ResponseEntity(employeeService.getAllEmployees(), HttpStatus.OK);
+	}
+
+	// Punto de petición get para obtener empleado por id
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	public ResponseEntity<EmployeeEntity> get(@PathVariable("id") Long id) throws RecordNotFoundException {
+		EmployeeEntity employee = employeeService.getEmployeeById(id);
+		return new ResponseEntity<>(employee, HttpStatus.OK);
+	}
+
+	// Punto de petición post para crear o actualizar un empleado
+	@RequestMapping(path = "/createorupdate", method = RequestMethod.POST, consumes = "application/json")
+	public ResponseEntity<EmployeeEntity> createOrUpdate(@RequestBody EmployeeEntity employee) {
+		return new ResponseEntity<>(employeeService.createOrUpdateEmployee(employee), HttpStatus.OK);
+	}
+
+	// punto de petición delete para borrar y devolver listado de empleados
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<List<EmployeeEntity>> delete(@PathVariable("id") Long id) throws RecordNotFoundException {
+		employeeService.deleteEmployeeById(id);
+		return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
 	}
 
 }
