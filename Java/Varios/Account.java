fc0120ca00cbe42f0a0d48d56489b483ae644bf0
JulianCMoralesAgudelo <juliancmorales10@gmail.com>
2020-12-11 19:26:51 -0500

    HTML5V8

---
+++ Java/Varios/Account.java
@@ -0,0 +1,14 @@
+class Account {
+    private int accountNumber;
+    private int accountBalance;
+
+    // getter method
+    public int getBalance() {
+        return this.account_balance;
+    }
+
+    // setter method
+    public void setNumber(int num) {
+        this.account_number = num;
+    }
+}
