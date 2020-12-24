95bb02362a000c288cee6acf30b61e4ca588214a
JulianCMoralesAgudelo <juliancmorales10@gmail.com>
2020-12-24 01:56:32 -0500

    listaDeCorreos.py

--- Python/Varios/filtrarCorreos.py
+++ Python/Varios/filtrarCorreos.py
@@ -1,26 +1,38 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import time
 import re
 
 # Cadena con los correos
-cadena = """
-    """
-
+cadena = """    """
 # Se eliminan caracteres externos al utf8
 cadena = cadena.encode('ascii', 'ignore').decode('ascii')
-
 
 # Expresion regular para filtrar los correos
 mailsearch = re.compile(r'[\w\-\.]+@[\w\-\.]+\.+[a-zA-Z]{1,4}')
 
 # Lista para almacenar los correos
-foundMail = []
+emails = []
 
 #  Bucle para recorrer la cadena y extraer los correos
 for mail in mailsearch.findall(cadena):
-    foundMail.append(mail)
+    emails.append(mail)
 
-#  Bucle para Imprimir resultados
-for mail in foundMail:
+# Eliminar correos repetidos.
+emails = set(emails)
+
+#  Bucle para Imprimir  y escribir resultados
+
+for mail in emails:    
     print(mail)
+
+# Imprimir cantidad de correos en el conjunto
+print(len(emails))
+
+# Organizar correos
+sorted(emails)
+
+fic = open("correos.txt", "w")
+fic.writelines("%s\n" % s for s in emails)
+fic.close()
+
