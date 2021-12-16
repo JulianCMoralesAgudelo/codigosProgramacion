4d6b532cf737ff7670e45e81ff6a76f72b6fdb6c
julian <juliancmorales10@gmail.com>
2021-12-15 23:05:51 -0500

    proyecto terminado

--- Python/Varios/busquedabinaria.py
+++ Python/Varios/busquedabinaria.py
@@ -18,10 +18,45 @@ def busqueda_lineal(lista,objetivo):
             return i
     return -1
 
-def busqueda_binaria(lista,objetivo,limite_inferir=None,limite_superir=None):
-    
+def busqueda_binaria(lista, objetivo, limite_inferior=None, limite_superior=None):
+    if limite_inferior is None:
+        limite_inferior = 0 # Inicio lista.
+    if limite_superior is None:
+        limite_superior = len(lista) - 1 # Fin lista.
+    if limite_superior < limite_inferior:
+        return -1
+
+    punto_medio = (limite_inferior + limite_superior) // 2
+
+    if lista[punto_medio] == objetivo:
+        return punto_medio
+    elif objetivo < lista[punto_medio]:
+        return busqueda_binaria(lista,objetivo, limite_inferior, punto_medio-1)
+    else:
+        return busqueda_binaria(lista,objetivo, punto_medio+1,limite_superior)
+
+if __name__=='__main__':
+    # Crear lista ordenada con diezmil numero aleatorios
+    longitud = 10000
+    conjunto_inicial = set()
+
+    while len(conjunto_inicial) < longitud:
+        conjunto_inicial.add(random.randint(-3 * longitud,3 * longitud))
+
+    lista_ordenada = sorted(list(conjunto_inicial))
+
+    # Medir tiempo de respuesta por busqueda lineal
+    inicio = time.time()
+    for objetivo in lista_ordenada:
+        busqueda_lineal(lista_ordenada,objetivo)
+    fin = time.time()
+    print(f"Tiempo de busqueda lineal: {(fin - inicio):.2f} segundos")
+    # Medir tiempo de respuesta por busqueda binaria
+
+    inicio = time.time()
+    for objetivo in lista_ordenada:
+        busqueda_binaria(lista_ordenada, objetivo)
+    fin = time.time()
+    print(f"Tiempo de busqueda binaria: {(fin - inicio):.2f} segundos")
 
 
-lista = [1,3,5,10,12]
-
-print(busqueda_lineal(lista,10))
